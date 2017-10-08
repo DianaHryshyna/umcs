c494e5b52ddb2d5625b1ea496aaee5e942d5bb26
Diana <32524600+DianaHryshyna@users.noreply.github.com>
2017-10-08 21:39:10 +0300

    .

---
+++ homework1/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float a;
+    float b;
+    float c;
+    scanf("%f", &a);
+    scanf("%f", &b);
+    scanf("%f", &c);
+    float V;
+    V = a * b * c;
+    printf("%f", V);
+    return 0;
+}
